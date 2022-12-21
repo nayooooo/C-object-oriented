b530cc38ce07a64e2785c047f642bd53d7e7d31d
nayooooo <97676318+nayooooo@users.noreply.github.com>
2022-12-21 11:30:38 +0800

    Delete build directory

--- build/CMakeFiles/test_01.dir/src/polymorphic.obj.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/test_01.dir/src/polymorphic.obj: \
- D:\code\C\VScodeProgram\project\test_01\src\polymorphic.c \
- D:/code/C/VScodeProgram/project/test_01/include/polymorphic.h \
- D:/code/C/VScodeProgram/project/test_01/include/config.h \
- D:/code/C/VScodeProgram/project/test_01/include/class.h
